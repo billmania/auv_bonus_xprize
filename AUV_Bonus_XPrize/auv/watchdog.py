8133528b3dbb6f2ca026b5abda682d773a1bab22
Bill Mania <billm@aescape.co>
2019-01-22 15:48:38 -0800

    In watchdog.py:         Removed the logging config which was in there by         mistake.

--- AUV_Bonus_XPrize/auv/watchdog.py
+++ AUV_Bonus_XPrize/auv/watchdog.py
@@ -9,10 +9,6 @@ from serial import Serial, SerialException, SerialTimeoutException
 from serial import EIGHTBITS, PARITY_NONE, STOPBITS_ONE
 from auv_bonus_xprize.settings import config
 
-logging.basicConfig(filename=config['DEFAULT']['logfile'],
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    level=logging.DEBUG)
-
 START = '$'
 END = '\n'
 SUCCESS = 'SUCCESS'
